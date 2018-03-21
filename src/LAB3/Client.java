ee29bdca155d627ac295297ab39a35a91f104369
Eunice <echinchilla22@yahoo.com>
2018-03-21 01:00:13 -0700

    initial commit

---
+++ src/LAB3/Client.java
@@ -0,0 +1,24 @@
+package LAB3;
+
+
+public class Client
+{
+    DFS dfs;
+    public Client(int p) throws Exception {
+        dfs = new DFS(p);
+
+            // User interface:
+            // join, ls, touch, delete, read, tail, head, append, move
+            // menu
+
+    }
+
+    static public void main(String args[]) throws Exception
+    {
+        if (args.length < 1 ) {
+            throw new IllegalArgumentException("Parameter: <port>");
+        }
+        Client client=new Client( Integer.parseInt(args[0]));
+
+     }
+}
