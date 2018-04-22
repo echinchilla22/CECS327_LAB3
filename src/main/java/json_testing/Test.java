21fa2ed9efe61da4dcd45a0e6f8e8f34e6ed0a05
Eunice <echinchilla22@yahoo.com>
2018-04-21 18:30:37 -0700

    testing

--- src/main/java/json_testing/Test.java
+++ src/main/java/json_testing/Test.java
@@ -5,12 +5,12 @@ public class Test {
         Metadata metadata = new Metadata();
         metadata.addFile("testFile", 0L);
         metadata.addFile("testFile2", 0L);
-
-        metadata.addChunkToFile("testFile", 10L, 1L);
-        metadata.addChunkToFile("testFile", 15L, 2L);
-        metadata.addChunkToFile("testFile2", 20L, 3L);
-        metadata.addChunkToFile("testFile2", 25L, 4L);
-        metadata.addChunkToFile("testFile2", 25L, 4L);
+//
+//        metadata.addChunkToFile("testFile", 10L, 1L);
+//        metadata.addChunkToFile("testFile", 15L, 2L);
+//        metadata.addChunkToFile("testFile2", 20L, 3L);
+//        metadata.addChunkToFile("testFile2", 25L, 4L);
+//        metadata.addChunkToFile("testFile2", 25L, 4L);
 
         //TODO: test the json here
 
